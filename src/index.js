7c4e04736f45ea16ddcd92b5a7d4cc10bcf3a217
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-03-26 23:16:51 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+app.use(cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = ['http://localhost:5173'];
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true
+  }));
+app.use(express.json());
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}));
+
+
+
+app.get("/", (req, res) => {
+    res.send({message: "Hello World"});
+});
+
+
+const startServer = async () => {
+    try {
+        await connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log("Server started on http://localhost:8080"));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer();
